025367c34be11f18251583877854a1f34fe9f596
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-09-25 13:33:55 -0300

    Ajuste de velocidade e deslize

--- gamejam/scripts/jogador_corrida_base.gd
+++ gamejam/scripts/jogador_corrida_base.gd
@@ -3,30 +3,51 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const DELIZE_VELOCITY = 500.0
+const VELOCITY_MAX = 700.00
 
 var direction
 var morreu: bool = false
+var isdeslizando: bool = false
+var ispulando: bool = false
 
 @export var input_prefix: String = "p1_"   # cada jogador muda isso no Inspector
 @export var spawn_point: Vector2 = Vector2(0, 0)
 
 @onready var player: AnimatedSprite2D = $Jogador
+@onready var deslize_timer: Timer = $Timers/DeslizeTimer
 
 func _physics_process(delta: float) -> void:
-	directions()
+	directions(delta)
 	jump(delta)
+	deslizar()
 	move_and_slide()
 
 func jump(delta: float) -> void:
+	if isdeslizando:
+		return
 	# Handle jump.
 	if Input.is_action_just_pressed(input_prefix + "pular") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+		ispulando = true
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		ispulando = false
+
+func deslizar():
+	if ispulando or isdeslizando:
+		return
+	if Input.is_action_just_pressed(input_prefix + "deslize"):
+		player.play("deslize")
+		isdeslizando = true
+		velocity.y = DELIZE_VELOCITY
+		deslize_timer.start()
 		
-func directions():
+func directions(delta):
+	if isdeslizando:
+		return
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_axis(input_prefix + "esquerda", input_prefix + "direita")
@@ -37,7 +58,10 @@ func directions():
 		player.play("run")
 	else:
 		player.play("idle")
-	velocity.x = direction * SPEED if direction else move_toward(velocity.x, 0, SPEED)
+	if direction:
+		if velocity.x < VELOCITY_MAX and velocity.x > -VELOCITY_MAX:
+			velocity.x += direction * SPEED * delta * 2
+	else: velocity.x = 0
 	
 func morre():
 	if morreu:
@@ -45,3 +69,7 @@ func morre():
 	morreu = true
 	player.play("die")
 	queue_free()
+
+func _on_deslize_timer_timeout() -> void:
+	isdeslizando = false
+	player.play("idle")
