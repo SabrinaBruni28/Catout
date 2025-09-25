7df9d671195a58d78415b548735ffd74ac5ebe1d
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-09-24 22:04:23 -0300

    Excluindo scripts que já não são mais necessários

--- gamejam/scripts/jogador_1.gd
+++
@@ -1,6 +0,0 @@
-extends PlayerBase
-
-func _ready() -> void:
-	input_prefix = "p1_"        # muda só o prefixo
-	spawn_point = Vector2(200, 94)
-	super._ready()              # chama o ready do base
--- gamejam/scripts/jogador_2.gd
+++
@@ -1,6 +0,0 @@
-extends PlayerBase
-
-func _ready() -> void:
-	input_prefix = "p2_"
-	spawn_point = Vector2(600, 94)
-	super._ready()
