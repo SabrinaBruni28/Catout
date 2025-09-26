a25b61fcebcbdb9f53dbacc88cebf7ba50e90f27
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-09-25 21:28:31 -0300

    Objetos lançados somem e não podem ser pegos

--- gamejam/scripts/objetos.gd
+++ gamejam/scripts/objetos.gd
@@ -3,9 +3,11 @@ extends RigidBody2D
 @export var throw_force: float = 1000.0
 @export var altura: float = 500.0
 @export var throw_direction: Vector2 = Vector2(1, -1)  # padrão, pode editar no Inspector
+@onready var camera: Camera2D = $"../../Camera2D"
 
 var held_by: Node = null
 var is_held: bool = false
+var lancado: bool = false
 
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
@@ -19,8 +21,18 @@ func _physics_process(delta: float) -> void:
 		global_position = held_by.global_position + Vector2(held_by.scale.x, held_by.scale.y * -18)
 		linear_velocity = Vector2.ZERO
 		angular_velocity = 0
+		
+	if lancado:
+		var viewport_rect = Rect2(
+			camera.global_position - camera.get_viewport_rect().size * 0.5 * camera.zoom,
+			camera.get_viewport_rect().size * camera.zoom
+		)
+		if not viewport_rect.has_point(global_position):
+			queue_free()
 
 func pick_up(player: Node) -> void:
+	if lancado:
+		return
 	is_held = true
 	held_by = player
 	collision_shape.disabled = true
@@ -28,6 +40,7 @@ func pick_up(player: Node) -> void:
 	set_collision_mask_value(1, false)   # não colide com nada
 
 func throw_item() -> void:
+	lancado = true
 	is_held = false
 	held_by = null
 	collision_shape.disabled = false
