c4da6fb7dd2a4eee718650b24cec1c27c8599638
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-09-26 04:39:20 -0300

    Música introdução

--- gamejam/scripts/inicial_screen.gd
+++ gamejam/scripts/inicial_screen.gd
@@ -2,14 +2,17 @@ extends Control
 
 @onready var hover_sound: AudioStreamPlayer2D = $Audios/HoverSound
 @onready var click_sound: AudioStreamPlayer2D = $Audios/ClickSound
+@onready var music: AudioStreamPlayer2D = $Audios/Music
 
 func _on_button_1_pressed() -> void:
 	click_sound.play()
+	music.stop()
 	await  click_sound.finished
 	get_tree().change_scene_to_file("res://scenes/FaseCorrida/fase_corrida.tscn")
 
 func _on_button_2_pressed() -> void:
 	click_sound.play()
+	music.stop()
 	await  click_sound.finished
 	get_tree().quit()
 
