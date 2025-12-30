2cb87d8fe35c26e3116c112d2785aa7624fc3510
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-12-30 18:48:50 -0300

    Ajuste de música

--- catout/scripts/componentes/audios.gd
+++ catout/scripts/componentes/audios.gd
@@ -3,3 +3,6 @@ extends Node
 @onready var hover_sound: AudioStreamPlayer2D = $HoverSound
 @onready var click_sound: AudioStreamPlayer2D = $ClickSound
 @onready var music: AudioStreamPlayer2D = $Music
+
+func _on_music_finished() -> void:
+	music.play()
