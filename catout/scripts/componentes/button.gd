b2558c19d4e53bcf798473609a8b1d0ac78053d8
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-12-31 17:25:32 -0300

    ajuste de botão

--- catout/scripts/componentes/button.gd
+++ catout/scripts/componentes/button.gd
@@ -63,28 +63,17 @@ func atualizar_texto():
 	if eventos.is_empty():
 		text = "Não configurado"
 	else:
-		text = limitar_texto(eventos[0].as_text(), 15)
+		text = limitar_texto(eventos[0].as_text(), 20)
 
 func limitar_texto(texto: String, limite_por_linha: int) -> String:
-	var max_chars := limite_por_linha * 3
-
-	# Cabe tudo em uma linha
 	if texto.length() <= limite_por_linha:
 		return texto
 
-	# Cabe em até 3 linhas sem cortar
-	if texto.length() <= max_chars:
-		var linhas := []
-		for i in range(0, texto.length(), limite_por_linha):
-			linhas.append(texto.substr(i, limite_por_linha))
-		return "\n".join(linhas)
+	var primeira_linha := texto.substr(0, limite_por_linha)
 
-	# Texto grande demais → corta e adiciona …
-	var linhas := []
-	for i in range(0, limite_por_linha * 2, limite_por_linha):
-		linhas.append(texto.substr(i, limite_por_linha))
+	if texto.length() <= limite_por_linha * 2:
+		var segunda_linha := texto.substr(limite_por_linha)
+		return primeira_linha + "\n" + segunda_linha
 
-	var ultima := texto.substr(limite_por_linha * 2, limite_por_linha - 1) + "…"
-	linhas.append(ultima)
-
-	return "\n".join(linhas)
+	var segunda_linha := texto.substr(limite_por_linha, limite_por_linha - 1) + "…"
+	return primeira_linha + "\n" + segunda_linha
